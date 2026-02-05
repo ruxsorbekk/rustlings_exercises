35d36e276346876581809d56dc4ccea6c07dc5c3
ruxsorbek <ruhsorbeknorimmatov588@gmail.com>
2026-02-05 22:14:53 +0500

    move semantics done

---
+++ move_semantics/src/main.rs
@@ -0,0 +1,13 @@
+mod mv1;
+mod mv2;
+mod mv3;
+mod mv4;
+mod mv5;
+
+fn main() {
+    // mv1::main();
+    // mv2::main();
+    // mv3::main();
+    // mv4::main();
+    mv5::main();
+}
---
+++ move_semantics/src/mv1.rs
@@ -0,0 +1,23 @@
+fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
+
+    vec.push(88);
+
+    vec
+}
+
+pub fn main() {
+    println!("{:?}", fill_vec(vec![]));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn move_semantics1() {
+        let vec0 = vec![22, 44, 66];
+        let vec1 = fill_vec(vec0);
+        assert_eq!(vec1, vec![22, 44, 66, 88]);
+    }
+}
